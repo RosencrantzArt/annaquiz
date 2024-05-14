0015be1477030108d0335d41fa514602e4422903
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-05-14 08:13:34 +0000

    add point at html to javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+//Point at html elements
+const startButton = document.getElementById('start-button');
+const nextQuestionButton =document.getElementById('next-question');
+const timerElement =document.getElementById('timer');
+let currentQuestionIndex = 0;
+let timerInterval;
+
